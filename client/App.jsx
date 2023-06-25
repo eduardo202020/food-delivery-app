6951a236e3d69115e85a1bf50ec62d497b56dda8
Eduardo26 <jguevaral@uni.pe>
2023-06-25 13:44:28 -0500

    Set up constants, dummy screens, dummy components, images, navigation and theme

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import Navigation from "./navigation";
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <Text className="text-sky-500 text-3xl text-center">hi from App</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <Navigation />;
 }
