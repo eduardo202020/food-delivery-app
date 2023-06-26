d1bd73b087441bcaf7972dbff53e2fe6573b7897
Eduardo26 <jguevaral@uni.pe>
2023-06-25 21:11:59 -0500

    Delivery and PreparingOrden Screens

--- client/screens/DeliveryScreen.jsx
+++ client/screens/DeliveryScreen.jsx
@@ -1,10 +1,94 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+
+import MapView, { Marker } from "react-native-maps";
+import * as Icon from "react-native-feather";
+
+import { featured } from "../constants";
+import { themeColors } from "../theme";
+
+import { useNavigation } from "@react-navigation/native";
 
 const DeliveryScreen = () => {
+  const restaurant = featured.restaurants[0];
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
+    <View className="flex-1">
+      {/* map view */}
+
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.lng,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        className="flex-1"
+        mapType="standard"
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.lng,
+          }}
+          title={restaurant.name}
+          description={restaurant.description}
+          pinColor={themeColors.bgColor(1)}
+        />
+      </MapView>
+      <View className="rounded-t-3xl -mt-12 bg-white relative">
+        <View className="flex-row justify-between px-5 pt-10">
+          <View>
+            <Text className="text-lg text-gray-700 font-semibold">
+              Estimated Arrival
+            </Text>
+            <Text className="text-3xl font-extrabold text-gray-700">
+              20-30 Minutes
+            </Text>
+            <Text className="mt-2 text-gray-700 font-semibold">
+              Your order is own its way!
+            </Text>
+          </View>
+          <Image
+            className="w-24 h-24"
+            source={require("../assets/images/bikeGuy2.gif")}
+          />
+        </View>
+        <View
+          style={{ backgroundColor: themeColors.bgColor(0.8) }}
+          className="p-2 flex-row justify-between items-center rounded-full my-5 mx-2"
+        >
+          <View className="p-1 rounded-full bg-white/40">
+            <Image
+              className="h-16 w-16 rounded-full"
+              source={require("../assets/images/deliveryGuy.png")}
+            />
+          </View>
+          <View className="flex-1 ml-3">
+            <Text className="text-lg font-bold text-white">Syed Noman</Text>
+            <Text className="font-semibold text-white">Your Rider</Text>
+          </View>
+          <View className="flex-row items-center space-x-3 mr-3">
+            <TouchableOpacity className="bg-white p-2 rounded-full">
+              <Icon.Phone
+                fill={themeColors.bgColor(1)}
+                strokeWidth={1}
+                stroke={themeColors.bgColor(1)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Home")}
+              className="bg-white p-2 rounded-full"
+            >
+              <Icon.X
+                fill={themeColors.bgColor(1)}
+                strokeWidth={5}
+                stroke={"red"}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- client/screens/PreparingOrderScreen.jsx
+++ client/screens/PreparingOrderScreen.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Image } from "react-native";
+import React, { useEffect } from "react";
+
+import { useNavigation } from "@react-navigation/native";
 
 const PreparingOrderScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      // move to delivery sceen
+      navigation.navigate("Delivery");
+    }, 3000);
+  }, []);
   return (
-    <View>
-      <Text>PreparingOrderScreen</Text>
+    <View className="flex-1 bg-white justify-center items-center">
+      <Image
+        source={require("../assets/images/delivery.gif")}
+        className="w-80 h-80"
+      />
     </View>
   );
 };
